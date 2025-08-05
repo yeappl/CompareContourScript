8420284c83cb6077e08b070053ed1238a8f36bb0
iStingerX <96965424+wedgieang@users.noreply.github.com>
2025-08-05 15:10:22 +0800

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using CompareContourScript;
+using System;
+using System.Windows;
+using VMS.TPS;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        public void Execute(ScriptContext context)
+        {
+            if (context.Patient == null)
+            {
+                MessageBox.Show("No patient is loaded.", "Error");
+                return;
+            }
+
+            Window window = new CompareContourWindow(context);
+            window.ShowDialog();
+
+        }
+    }
+}
---
+++ CompareContourWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VMS.TPS.Common.Model.API;
+
+
+namespace CompareContourScript
+{
+    /// <summary>
+    /// Interaction logic for CompareContourWindow.xaml
+    /// </summary>
+    public partial class CompareContourWindow : Window
+    {
+        private ScriptContext _context; 
+
+        public CompareContourWindow(ScriptContext context)
+        {
+            InitializeComponent();
+            _context = context; 
+
+            var patient = context.Patient;
+
+            if (patient != null)
+            {
+                PatientDetailsText.Text = "Patient Name: " + patient.FirstName + " " + patient.LastName + "\n" + "Patient ID: " + patient.Id;
+            }
+
+            if (_context.StructureSet != null)
+            {
+                foreach (var structureSet in _context.Patient.StructureSets)
+                {
+                    StructureSetComboBox1.Items.Add(structureSet.Id);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No structure set is loaded for this patient.", "Error");
+            }
+            
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
